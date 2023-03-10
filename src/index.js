9ff4df797dfc63ef6d4f7e706c01eb05d84dec2e
AndreyDerunow <derunov.andreyy@gmail.com>
2023-03-10 15:41:46 +0400

    master's changes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,49 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useEffect, useState } from "react";
+import ReactDOM from "react-dom/client";
+import * as actions from "./store/actions";
+import { initiateStore } from "./store/store";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const store = initiateStore();
+
+const App = () => {
+    const [state, setState] = useState(store.getState());
+    useEffect(() => {
+        store.subscribe(() => {
+            setState(store.getState());
+        });
+    }, []);
+    const completeTask = (elId) => {
+        store.dispatch(actions.taskCompleted(elId));
+    };
+    const changeTitle = (taskId) => {
+        store.dispatch(actions.titleChanged(taskId));
+    };
+    return (
+        <>
+            <h1>App</h1>
+
+            <ul>
+                {state.map((el) => (
+                    <li key={el.id}>
+                        <p>{el.title}</p>
+                        <p>{el.completed ? "completed" : "not done"}</p>
+                        <button onClick={() => completeTask(el.id)}>
+                            Complete
+                        </button>
+                        <button onClick={() => changeTitle(el.id)}>
+                            Change Title
+                        </button>
+                        <hr></hr>
+                    </li>
+                ))}
+            </ul>
+        </>
+    );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
